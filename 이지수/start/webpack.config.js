3e94b324694a3c5ffd5edcd1595f3c1d0eb94af5
Lee-ji-soo <nbsp3@naver.com>
2020-08-18 21:01:18 +0900

    setting

---
+++ 이지수/start/webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    entry: [
+        "./src/index.js"
+    ],
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "bable-loader",
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }
+        ]
+    },
+    resolve: {
+        extensions: ["*", ".js", "jsx"]
+    },
+    output: {
+        path: __dirname + "/dist",
+        publicPath: "/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: "./dist",
+        port: 3000
+    }
+
+}
