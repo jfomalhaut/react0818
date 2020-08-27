817fd29d37ed2112ebf7dce2753e1bcc887b9de4
Lee-ji-soo <nbsp3@naver.com>
2020-08-27 09:23:20 +0900

    SPA 회사,연혁,CEO 페이지 구현

--- 이지수/start/src/App.js
+++ 이지수/start/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import Home from "./routers/Main";
-import Main from './routers/Home';
-import List from './routers/List';
-import Navigation from './components/Navigation';
+import React from 'react'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import Intro from './routers/Intro'
+import History from "./routers/History"
+import Ceo from './routers/Ceo'
+import Navigation from './components/Navigation'
 
 const App = () => {
     return (
         <BrowserRouter>
             <Navigation />
             <Switch>
-                <Route path="/" exact={true} component={Home} />
-                <Route path="/main" component={Main} />
-                <Route path="/list" component={List} />
+                <Route path="/intro" exact={true} component={Intro} />
+                <Route path="/history" component={History} />
+                <Route path="/ceo" component={Ceo} />
                 <Redirect path="*" to="/" />
             </Switch>
         </BrowserRouter>
--- 이지수/start/src/components/Navigation.js
+++ 이지수/start/src/components/Navigation.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
     return (
         <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/main'>Main</Link></li>
-            <li><Link to='/list'>List</Link></li>
+            <li><Link to='/intro'>회사소개</Link></li>
+            <li><Link to='/history'>연혁</Link></li>
+            <li><Link to='/ceo'>대표메세지</Link></li>
         </ul>
     )
 }
 
-export default Navigation;
+export default Navigation
