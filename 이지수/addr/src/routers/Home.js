1e0eb95cf02644298a54f89316d8c2003f2e3b31
이준 <dlwnssla@jfomalhaut.com>
2020-09-04 20:45:32 +0900

    dd

--- 이지수/addr/src/routers/Home.js
+++ 이지수/addr/src/routers/Home.js
@@ -1,46 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import Axios from 'axios'
-const ConfmKey = process.env.CONFIRM_KEY
-const Request_URL = 'http://www.juso.go.kr/addrlink/addrLinkApi.do'
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+const ConfmKey = process.env.CONFIRM_KEY;
+const Request_URL = 'http://www.juso.go.kr/addrlink/addrLinkApi.do';
 
 const Home = () => {
-    const [list, setList] = useState([])
-    const [keyword, setKeyword] = useState('')
-    const [keyword2, setKeyword2] = useState('')
-    const [total, setTotal] = useState('')
-    const [page, setPage] = useState(1)
+    const [list, setList] = useState([]);
+    const [keyword, setKeyword] = useState('');
+    const [keyword2, setKeyword2] = useState('');
+    const [total, setTotal] = useState('');
+    const [page, setPage] = useState(1);
     const search = () => {
         Axios.get(`${Request_URL}?confmKey=${ConfmKey}&currentPage=${page}&countPerPage=10&resultType=json&keyword=${keyword2}`)
             .then(res => {
-                const { data: { results: { juso, common: { totalCount } } } } = res
-                setTotal(totalCount)
-                setList(juso)
-            })
-    }
+                const { data: { results: { juso, common: { totalCount } } } } = res;
+                setTotal(totalCount);
+                setList(juso);
+            });
+    };
 
     const onChangeKeyword = (ev) => {
         const { target: { value } } = ev;
-        setKeyword(value)
+        setKeyword(value);
     }
 
     const plusPage = () => {
         if (page >= 1) {
-            setPage(page + 1)
-            return
+            setPage(page + 1);
+            return;
         }
-    }
+    };
+
     const minusPage = () => {
         if (page > 1) {
-            setPage(page - 1)
-            return
+            setPage(page - 1);
+            return;
         }
-    }
+    };
 
     useEffect(() => {
-        if (keyword2) {
-            search()
-        }
-    }, [keyword2, page])
+        console.log(list);
+        // if (keyword2) {
+            search();
+        // }
+    }, [keyword2]);
 
     return (
         <div className='container'>
